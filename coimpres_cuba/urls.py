72fddd74e213c1f7e43a49831769ed603bdedd76
CDTwilight <carlosd981022@gmail.com>
2026-03-21 06:19:51 -0400

    Add coimpres_cuba/urls.py

---
+++ coimpres_cuba/urls.py
@@ -0,0 +1,52 @@
+"""
+URL configuration for coimpres_cuba project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# coimpres_cuba/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from productos.sitemaps import StaticViewSitemap, ProductSitemap, CategorySitemap, ProveedorSitemap
+from . import views
+
+# Configuración de sitemaps para SEO
+sitemaps = {
+    'static': StaticViewSitemap,
+    'products': ProductSitemap,
+    'categories': CategorySitemap,
+    'suppliers': ProveedorSitemap,
+}
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
+    path('productos/', include('productos.urls')),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('change-language/', views.change_language, name='change_language'),
+    
+    # SEO URLs
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', views.robots_txt, name='robots_txt'),
+]
+
+# Servir archivos media y static en desarrollo Y producción
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # Para producción con WhiteNoise
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
